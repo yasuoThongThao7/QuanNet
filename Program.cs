893a294d5bda8d7cd2f25bd025d52f324e652ca1
Nguyễn Thanh Liem <liemlol0934@gmail.com>
2025-04-28 11:22:39 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using QuanNet.Models; 
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+
+
+// Cookie 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Users/Login"; // Khi chưa đăng nhập
+        options.AccessDeniedPath = "/Users/Login"; // Khi không đủ quyền
+        options.ExpireTimeSpan = TimeSpan.FromHours(2);//Cho tầm 2 h
+        options.SlidingExpiration = true;//Gia hạn khi đang dùng
+    });
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Kích hoạt xử lý phân quyền
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Routing mặc định
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
